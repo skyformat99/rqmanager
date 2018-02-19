375bcbd3462e125ced0e70a7d6a1214d95597743
Administrator <751957846@qq.com>
2018-02-19 22:17:27 +0800

    Initial commit

---
+++ src/main/java/www/mjxy/rq/manager/RqmanagerApplication.java
@@ -0,0 +1,12 @@
+package www.mjxy.rq.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RqmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RqmanagerApplication.class, args);
+	}
+}
---
+++ src/test/java/www/mjxy/rq/manager/RqmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package www.mjxy.rq.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RqmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
